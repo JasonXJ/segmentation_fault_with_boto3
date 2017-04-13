a95c141210c51f1210a494a86afdcd4133b68297
Jason <jason@autumncompass.com>
2017-04-13 17:03:35 +1000

    Add some comment

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -6,8 +6,12 @@ def foo():
     sys.exit(0)
 
 
+# Unable to reproduce if merge this function with foo()
 def bar(dynamodb):
-    import mypackage.justtyping
+    import mypackage.justtyping  # Unable to reproduce without this line
 
+    # Unable to reproduce if do not get item twice, or use `dynamodb` in both
+    # cases, or create new resource in both cases.
+    # Note that the table must exist, but it does not need to have the item.
     dynamodb.Table('sigsegv').get_item(Key={'pkey': 'xxx'})
     boto3.resource('dynamodb', region_name='us-east-2').Table('sigsegv').get_item(Key={'pkey': 'xxx'})
--- mypackage/justtyping.py
+++ mypackage/justtyping.py
@@ -1,2 +1,2 @@
 from typing import Sequence
-x = Sequence['xxx']
+x = Sequence['xxx']  # Unable to reproduce without this line, or replace string `'xxx'` with a class (e.g. `bytes`)
